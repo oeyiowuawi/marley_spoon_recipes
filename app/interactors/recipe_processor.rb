0b683116c2e228f5ab98bf4fb010fb0b6a862bb4
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2020-09-08 23:17:27 +0100

    RecipeProcessor spec passes

--- app/interactors/recipe_processor.rb
+++ app/interactors/recipe_processor.rb
@@ -1,8 +1,21 @@
 class RecipeProcessor
   attr_reader :recipe_count
 
+  def initialize
+    @contentful_service = ContentfulService.new
+  end
+
 
   def call
+    fetch_recipes
+  end
+
+  private
+
+  attr_reader :contentful_service, :recipes
+
+  def fetch_recipes
+    @recipes = contentful_service.fetch_recipes
   end
 
 end
