47c6a039558fc2dcc1b0196d6d8409a15c7bc3db
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2020-09-09 14:47:29 +0100

    add read_all to recipe_data_store

--- app/models/recipe_data_store.rb
+++ app/models/recipe_data_store.rb
@@ -9,6 +9,12 @@ class RecipeDataStore
     db.set(data[:id], data.to_json)
   end
 
+  def read_all
+    key_store = []
+    retrieve_all_keys(key_store)
+    db.mget(key_store.flatten).compact
+  end
+
   private
 
   attr_reader :db
@@ -16,6 +22,25 @@ class RecipeDataStore
   def build_redis_store
     redis_connection = Redis.new
     Redis::Namespace.new(:marley_spoon, redis: redis_connection)
+  end
+
+  def retrieve_all_keys(key_store, cursor: 0)
+    cursor, keys = db.scan(cursor, match: "*")
+    if keys.empty?
+      []
+    else
+      process_scan_result(key_store, keys, cursor)
+        
+    end
+  end
+
+  def process_scan_result(key_store, keys, cursor)
+    key_store << keys
+    if cursor.to_i.zero?
+      key_store
+    else
+      retrieve_all_keys(key_store, cursor: cursor)
+    end
   end
 
 end
--- spec/models/recipe_data_store_spec.rb
+++ spec/models/recipe_data_store_spec.rb
@@ -14,4 +14,25 @@ describe RecipeDataStore do
       expect(db).to have_received(:set).with("Ljbd78", data.to_json)
     end
   end
+
+  describe "#read_all" do
+    it "returns all the data saved" do
+      db = build_redis_connection
+      db.set("1L3k15", {id: "1L3k15", description: "test data"}.to_json)
+      store = RecipeDataStore.new(db: db)
+
+      result = store.read_all
+
+      expect(result).to contain_exactly(
+        {id: "1L3k15", description: "test data"}.to_json,
+      )
+
+      db.flushdb
+    end
+  end
+
+  def build_redis_connection
+    redis_connection = Redis.new
+    Redis::Namespace.new(:marley_spoon_test, redis: redis_connection)
+  end
 end
