dca147ed67fb4f468fb4050653fbd131904e9baf
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2020-09-10 00:45:54 +0100

    more styles

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router'
 
-const Header = () =>(
-  <nav className="navbar fixed-to navbar-dark bg-dark justify-content-between">
-    <a className="navbar-brand">Marley Spoon Recipes</a>
-    <form className="form-inline">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search by recipe name" />
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </nav>
-)
+class Header extends Component {
 
-export default Header;
+  goToRecipeList = (event) => {
+    event.preventDefault();
+    this.props.history.push("/");
+  }
+
+  render() {
+    return(
+      <nav className="navbar fixed-to navbar-dark bg-dark justify-content-between">
+      <a className="navbar-brand" onClick={this.goToRecipeList}>Marley Spoon Recipes</a>
+      <form className="form-inline">
+        <input className="form-control mr-sm-2" type="search" placeholder="Search by recipe name" />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
+    )
+  }
+
+}
+
+export default withRouter(Header);
